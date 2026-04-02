cc795dcd3d075da223a7bfa904c42ab02b8166c7
mohamed_bnz <simobnz81@gmail.com>
2026-04-02 15:33:22 +0100

    Affichage des éléments du panier avec quantité

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,5 @@
 function setupCart() {
-  // Initialisation du panier
+  displayCart();
 }
 
 function displayCart() {
@@ -10,6 +10,14 @@ function displayCart() {
   if (cart.length === 0) {
     container.innerHTML = '<p>Panier vide</p>';
     return;
+  }
+
+  for (let i = 0; i < cart.length; i++) {
+    let item = cart[i];
+    let card = document.createElement('div');
+    card.innerHTML = '<div>' + item.id + '</div>' +
+                     '<div>Quantité: ' + item.quantity + '</div>';
+    container.appendChild(card);
   }
 }
 
