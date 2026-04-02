0638da6bd35c8a3b27281fa9be489c51dca8934a
mohamed_bnz <simobnz81@gmail.com>
2026-04-02 15:21:52 +0100

    Ajout de l'affichage dynamique des jeux

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 let selectedCategory = 'All';
 
+function displayGames(list) {
+  let grid = document.getElementById('gameGrid');
+  grid.innerHTML = '';
+
+  for (let i = 0; i < list.length; i++) {
+    let game = list[i];
+    let card = document.createElement('div');
+    card.textContent = game.title;
+    grid.appendChild(card);
+  }
+}
+
 function setup() {
   // Initialisation du store
 }
