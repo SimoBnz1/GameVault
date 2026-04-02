a9c80ac8d2bd1c5474b2d89f62f71b0086d3e12f
mohamed_bnz <simobnz81@gmail.com>
2026-04-02 14:37:21 +0100

    ajouter le file data.js qui contient les donnes

---
+++ js/data.js
@@ -0,0 +1,50 @@
+const games = [
+    {
+        id: 1,
+        title: "Cyberpunk 2077",
+        price: 59.99,
+        category: "RPG",
+        image: "https://cdn1.epicgames.com/offer/77f2b98e2cef40c8a7437518bf420e47/EGS_Cyberpunk2077PhantomLiberty_CDPROJEKTRED_DLC_S1_2560x1440-c62f1eb1498aaea2fc109b7aa50279a3?resize=1&w=480&h=270&quality=medium",
+        description: "Futuristic RPG in Night City"
+    },
+    {
+        id: 2,
+        title: "Elden Ring",
+        price: 49.99,
+        category: "Action",
+        image: "https://external-game-cover-image-cf.store.on.epicgames.com/119133?resize=1&w=360&h=480&quality=medium",
+        description: "Fantasy open world RPG"
+    },
+    {
+        id: 3,
+        title: "Modern Warfare III",
+        price: 69.99,
+        category: "FPS",
+        image: "https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/store/games/mw3/overview/Store_GamesPDP_Hero01.png?imwidth=1920",
+        description: "Military FPS action"
+    },
+    {
+        id: 4,
+        title: "The Witcher 3",
+        price: 29.99,
+        category: "RPG",
+        image: "https://cdn1.epicgames.com/offer/14ee004dadc142faaaece5a6270fb628/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S2_1200x1600-53a8fb2c0201cd8aea410f2a049aba3f?resize=1&w=360&h=480&quality=medium",
+        description: "Adventure RPG"
+    },
+    {
+        id: 5,
+        title: "FIFA 25",
+        price: 79.99,
+        category: "Sport",
+        image: "https://external-game-cover-image-cf.store.on.epicgames.com/314499?resize=1&w=360&h=480&quality=medium",
+        description: "Football simulation"
+    },
+    {
+        id: 6,
+        title: "Starfield",
+        price: 55.00,
+        category: "RPG",
+        image: "https://external-game-cover-image-cf.store.on.epicgames.com/96437?resize=1&w=360&h=480&quality=medium",
+        description: "Space exploration RPG"
+    }
+];
