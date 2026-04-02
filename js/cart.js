57c2fb311f44d7231233ef5546ad867a4f3e3eb3
mohamed_bnz <simobnz81@gmail.com>
2026-04-02 15:33:10 +0100

    Ajout de l'affichage initial du panier

--- js/cart.js
+++ js/cart.js
@@ -2,4 +2,15 @@ function setupCart() {
   // Initialisation du panier
 }
 
+function displayCart() {
+  let cart = getCart();
+  let container = document.getElementById('cartItems');
+  container.innerHTML = '';
+
+  if (cart.length === 0) {
+    container.innerHTML = '<p>Panier vide</p>';
+    return;
+  }
+}
+
 document.addEventListener('DOMContentLoaded', setupCart);
